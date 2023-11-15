c477f1b61c6adab963aaa6663a6abc7087f40447
Alona Kuznietsova <acvetochka@gmail.com>
2023-11-15 16:20:45 +0100

    change screens config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,17 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  screens: {
-    sm: '480px',
-    md: '768px',
-    xl: '1280px',
-    smOnly: { max: '767.98px' },
-    mdOnly: { min: '768px', max: '1279.98px' },
-    notXl: { max: '1279.98px' },
-  },
+
   theme: {
     extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        xl: '1280px',
+        smOnly: { max: '767.98px' },
+        mdOnly: { min: '768px', max: '1279.98px' },
+        notXl: { max: '1279.98px' },
+      },
       container: {
         center: true,
         padding: {
