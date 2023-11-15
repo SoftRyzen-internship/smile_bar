235e0d3bf839e468dd798fdbc02837cba1f8c9de
Tanyapavliuk <tanyapavlyk1998@gmail.com>
2023-11-15 14:17:27 +0200

    add title component

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+const Title = ({ children }) => {
+  return (
+    <h2 className="text-center text-stone-900 text-2xl font-bold uppercase md:text-[32px] lg:text-[40px]">
+      {children}
+    </h2>
+  );
+};
+
+export default Title;
