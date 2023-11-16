aaf0af5e88e578530f6d46de9fd4cf75adf1c000
Viacheslav Plevako <cheryec@ukr.net>
2023-11-16 18:40:50 +0200

    add Slider

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import LogoIcon from '/public/logo.svg';
 
 export const Logo = ({
   className = 'w-[68px] h-12 xl:w-[90px] xl:h-[62px]',
 }) => {
   return <LogoIcon className={className} />;
+};
+
+Logo.PropTypes = {
+  className: PropTypes.string,
 };
