d669a39075ba19570e816de9bb68eec10532f442
Tanyapavliuk <tanyapavlyk1998@gmail.com>
2023-11-17 19:08:02 +0200

    fix/title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,9 +1,12 @@
-const Title = ({ children }) => {
+import PropTypes from 'prop-types';
+
+export const Title = ({ children }) => {
   return (
     <h2 className="text-center text-primary text-2xl font-bold uppercase md:text-[32px] lg:text-[40px]">
       {children}
     </h2>
   );
 };
-
-export default Title;
+Title.propTypes = {
+  children: PropTypes.node.isRequired,
+};
