339cb489f6f4c7755a7180cbe45dd9b7444cc42d
Viacheslav Plevako <cheryec@ukr.net>
2023-11-19 20:04:12 +0200

    add Procedure Component

---
+++ src/sections/ProcedureSection/ProcedureSection.jsx
@@ -0,0 +1,15 @@
+import { Procedure } from '@/components/Procedure/Procedure';
+import { ProcedureList } from '@/components/ProcedureList/ProcedureList';
+import { Section } from '@/components/Section';
+import { Slider } from '@/components/Slider';
+import React from 'react';
+
+export const ProcedureSection = () => {
+  return (
+    <Section title="Title">
+      <Slider className="notCentral" centralMode={false}>
+        <ProcedureList />
+      </Slider>
+    </Section>
+  );
+};
