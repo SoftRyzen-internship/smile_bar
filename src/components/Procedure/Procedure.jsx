9753631db3862c55a9514880a0900e9c53631d17
Viacheslav Plevako <cheryec@ukr.net>
2023-11-21 15:48:28 +0200

    change side card

--- src/components/Procedure/Procedure.jsx
+++ src/components/Procedure/Procedure.jsx
@@ -16,7 +16,7 @@ export const Procedure = ({ service }) => {
   return (
     <div className=" group bg-transparent cursor-pointer [perspective:1000px]">
       <div className="relative mx-auto w-auto h-[502px] md:w-[342px] xl:w-[390px] xl:h-[482px] z-auto [transform-style:preserve-3d] xl:group-hover:[transform:rotateY(180deg)] duration-1000  ">
-        <div className="absolute [backface-visibility:hidden] [transform:rotateY(180deg)] w-full h-full inset-0">
+        <div className="absolute [backface-visibility:hidden] [transform:rotateY(180deg)] xl:[transform:rotateY(0deg)] w-full h-full inset-0">
           <div className="w-full h-full absolute z-10 bg-gradient-to-b from-white/0 to-white/75"></div>
           <Image
             className="object-cover rounded-3xl w-full h-full"
@@ -29,7 +29,7 @@ export const Procedure = ({ service }) => {
             {title}
           </p>
         </div>
-        <div className="flex flex-col justify-between p-9 bg-zinc-400 bg-opacity-10 rounded-3xl backdrop-blur-[25px] absolute inset-0 w-full h-full [backface-visibility:hidden] text-primary font-sans">
+        <div className="flex flex-col justify-between p-9 bg-zinc-400 bg-opacity-10 rounded-3xl backdrop-blur-[25px] absolute inset-0 w-full h-full xl:[transform:rotateY(180deg)] [backface-visibility:hidden] text-primary font-sans">
           <div className="flex-col  items-center  gap-6 inline-flex ">
             <div className="flex-col justify-start items-center gap-1 inline-flex">
               <p className="text-right  text-base uppercase tracking-[16px]">
