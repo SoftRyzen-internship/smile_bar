6e4c75f39bc59bb72d2f5f9bfbb8f444f3829f51
Viacheslav Plevako <cheryec@ukr.net>
2023-11-24 09:30:48 +0200

    fix bug

--- src/sections/ProcedureSection/ProcedureSection.jsx
+++ src/sections/ProcedureSection/ProcedureSection.jsx
@@ -8,31 +8,35 @@ import { getServices } from '../../../sanity/fetch';
 import procedure from '@/data/procedure.json';
 
 export const ProcedureSection = () => {
-  const [allService, setAllService] = useState('');
+  const [allService, setAllService] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const get = async () => {
       const getAllServices = await getServices();
       setAllService(getAllServices);
+      setIsLoading(false);
     };
-
+    setIsLoading(true);
     get();
   }, []);
 
   return (
-    <Section id="Procedure" title="ПОСЛУГИ">
-      <p className=" pt-4 text-center text-primary text-2xl">
-        {procedure.map(item => item.description)}
-      </p>
-      <Slider className="notCentral" centralMode={false} infinite={false}>
-        {allService &&
-          allService.map(service => (
-            <Procedure key={service.title} service={service} />
-          ))}
-      </Slider>
-      <p className="pt-4 text-center text-primary text-xl">
-        {procedure.map(item => item.promis)}
-      </p>
-    </Section>
+    !isLoading && (
+      <Section id="Procedure" title="ПОСЛУГИ">
+        <p className="md:w-[642px] xl:w-[860px] mx-auto pt-4 text-center text-primary text-lg md:text-xl xl:text-2xl">
+          {procedure.map(item => item.description)}
+        </p>
+        <Slider className="notCentral" centralMode={false} infinite={false}>
+          {allService.length > 0 &&
+            allService.map(service => (
+              <Procedure key={service.title} service={service} />
+            ))}
+        </Slider>
+        <p className="md:w-[623px] xl:w-[860px] mx-auto pt-4 text-center text-primary text-base xl:text-xl">
+          {procedure.map(item => item.promis)}
+        </p>
+      </Section>
+    )
   );
 };
