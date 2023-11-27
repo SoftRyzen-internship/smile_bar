6db2060312bac2b2ec0fe92d74c4947d1b47852a
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-11-28 00:49:52 +0200

    fix media rule in scroll

--- src/utils/scrollIfNeedForItem.js
+++ src/utils/scrollIfNeedForItem.js
@@ -1,7 +1,7 @@
 export const scrollIfNeedForItem = (element, checkMobile = false) => {
   const { y, height } = element.getBoundingClientRect();
   const checkIsMobile =
-    checkMobile && window && window.matchMedia('(max-width: 768px)').matches;
+    checkMobile && window && window.matchMedia('(max-width: 767px)').matches;
   console.log({ y, height, checkIsMobile });
   if (checkIsMobile || y < 0) {
     console.log('Scroll');
