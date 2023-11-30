3fc04af2b73824fc8cdf569840a0e1d88e8e5e00
Alona Kuznietsova <acvetochka@gmail.com>
2023-11-30 12:20:53 +0100

    change scroll

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import navigation from '@/data/navigation.json';
+import { Link } from 'react-scroll';
 
 export const Navigation = ({
   className = '',
@@ -10,13 +11,36 @@ export const Navigation = ({
     <ul className={`flex flex-col items-center gap-5 xl:flex-row ${className}`}>
       {navigation.map(({ id, title, link }, index) => (
         <li key={id}>
-          <a
-            href={`${link}`}
+          {isShow ? (
+            <a
+              href={`#${link}`}
+              onClick={isShow ? onClose : null}
+              className="text-center text-xl font-normal text-primary cursor-pointer xl:text-base hover:text-contact link"
+            >
+              {title}
+            </a>
+          ) : (
+            <Link
+              onClick={isShow ? onClose : null}
+              className="text-center text-xl font-normal text-primary cursor-pointer xl:text-base hover:text-contact link"
+              to={link}
+              spy={true}
+              smooth={true}
+              duration={300}
+              delay={500}
+              href="/"
+            >
+              {title}
+            </Link>
+          )}
+
+          {/* <a
+            href={`/${link}`}
             onClick={isShow ? onClose : null}
             className="text-center text-xl font-normal text-primary cursor-pointer xl:text-base hover:text-contact link"
           >
             {title}
-          </a>
+          </a> */}
         </li>
       ))}
     </ul>
