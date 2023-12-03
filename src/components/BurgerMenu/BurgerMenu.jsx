00639deea683154019ae7655c7b09df05bce6782
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-12-03 17:25:06 +0200

    fix burger menu album layout

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -10,7 +10,6 @@ import { forwardRef } from 'react';
 export const BurgerMenu = forwardRef(function menu({ isShow, onClose }, ref) {
   return (
     <div
-      ref={ref}
       className={`w-full top-0 left-0 xl:hidden fixed bg-white  gradient  h-screen z-[300] gradient transition duration-150 easy-in-out
       ${
         isShow
@@ -19,8 +18,11 @@ export const BurgerMenu = forwardRef(function menu({ isShow, onClose }, ref) {
       } 
       `}
     >
-      <div className="gradient w-full h-full pt-9 z-[400]">
-        <div className="container mx-auto">
+      <div
+        ref={ref}
+        className="gradient w-full h-full pt-9 z-[400] pb-20 overflow-auto"
+      >
+        <div className="container mx-auto ">
           <div className="mb-9 flex justify-between">
             <Logo isShowMenu={isShow} onCloseMenu={onClose} />
             <BurgerIconWrap setMenuShow={onClose} open={true}>
